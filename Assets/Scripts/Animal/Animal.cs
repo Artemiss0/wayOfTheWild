021b71253c058276e5fa838a474a346d68d36d76
Phone-game <darylvdberg@gmail.com>
2020-10-14 12:28:14 +0200

    Added animal drop system

---
+++ Assets/Scripts/Animal/Animal.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Animal : MonoBehaviour
+{
+    public GameObject AbilityPrefab;
+    protected string AbilityPath = "ScriptableObjects/Animal/Ability/";
+    public abstract void DropAbility();
+}
---
+++ Assets/Scripts/Animal/Squirrel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squirrel : Animal
+{
+    private void Start()
+    {
+        DropAbility();
+    }
+
+    public override void DropAbility()
+    {
+        Instantiate(AbilityPrefab, transform.position + transform.forward * 2, Quaternion.identity);
+        AnimalAbility squirrel = (AnimalAbility) Resources.Load(AbilityPath + AnimalAbilityNameEnum.Squirrel);
+        AbilityPrefab.GetComponent<AbilityDrop>().AnimalAbility = squirrel;
+    }
+}
