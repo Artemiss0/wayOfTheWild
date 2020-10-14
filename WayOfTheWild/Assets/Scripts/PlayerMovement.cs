98c0a5be3dcd483fe8a077e08e22cbd4d066b075
nienkeov <nm.overmeer@gmail.com>
2020-10-14 12:55:54 +0200

    Added player movement, implemented cinemachine

---
+++ WayOfTheWild/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController Controller;
+    public Transform cam;
+
+    public float Speed = 6f;
+    public float TurnSmoothTime = 0.1f;
+
+    private float turnSmoothVelocity;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, TurnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            Controller.Move(moveDir.normalized * Speed * Time.deltaTime);
+        }
+    }
+}
